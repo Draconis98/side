2a3bb684c80351a93506231ab4a4a369808632a6
draco <draco98h@gmail.com>
2023-09-09 17:53:21 +0800

    [database] Remove unused function InitDB()

--- database/init.go
+++ database/init.go
@@ -6,12 +6,6 @@ import (
 	"log"
 )
 
-func InitDB() {
-	// 初始化数据库连接
-	db := InitDBConnection()
-	defer db.Close()
-}
-
 func InitDBConnection() *sql.DB {
 	info := "side:Serve@123@tcp(10.30.19.15:3306)/side"
 	db, err := sql.Open("mysql", info)
