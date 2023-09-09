1ef30b8b1f2637c38643f6cd3b2f4b1f993f99d6
draco <draco98h@gmail.com>
2023-09-09 13:18:01 +0800

    [database/insert.go] Removed container_name column from INSERT statement. This change removes the need for a container name when inserting an image into the database.

--- database/insert.go
+++ database/insert.go
@@ -20,12 +20,12 @@ func InsertUser(db *sql.DB, userName string) {
 
 func InsertImage(db *sql.DB, imageName, userName string) {
 	commitTime := time.Now()
-	stmt, err := db.Prepare("INSERT INTO image(image_name, container_name, commit_time, user_name) VALUES(?, ?, ?, ?)")
+	stmt, err := db.Prepare("INSERT INTO image(image_name, commit_time, user_name) VALUES(?, ?, ?)")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	_, err = stmt.Exec(imageName, "", commitTime, userName)
+	_, err = stmt.Exec(imageName, commitTime, userName)
 	if err != nil {
 		log.Fatalln(err)
 	}
