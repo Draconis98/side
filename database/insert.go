5b9e5350192e26c2ab6ea1d6dcd3aa79a6634acb
draco <draco98h@gmail.com>
2023-09-09 13:21:45 +0800

    [database/insert.go] Change commitTime to use local time when inserting an image to the database

--- database/insert.go
+++ database/insert.go
@@ -19,7 +19,7 @@ func InsertUser(db *sql.DB, userName string) {
 }
 
 func InsertImage(db *sql.DB, imageName, userName string) {
-	commitTime := time.Now()
+	commitTime := time.Now().Local()
 	stmt, err := db.Prepare("INSERT INTO image(image_name, commit_time, user_name) VALUES(?, ?, ?)")
 	if err != nil {
 		log.Fatalln(err)
