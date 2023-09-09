56b51cf669c5488339edcf3f7936e908820b55fa
draco <draco98h@gmail.com>
2023-09-09 12:02:40 +0800

    [database/main.go] Update database connection settings and add error handling

--- database/main.go
+++ database/main.go
@@ -18,9 +18,17 @@ func main() {
 }
 
 func InitDBConnection() *sql.DB {
-	db, err := sql.Open("mysql", "root:Agileserve@123@tcp(10.30.19.15:3306)/side")
+	info := "root:Agileserve@123@tcp(10.30.19.15:3306)/side"
+	db, err := sql.Open("mysql", info)
 	if err != nil {
 		log.Fatalln(err) // connect to database failed
+	}
+
+	db.SetMaxOpenConns(35)
+	db.SetMaxIdleConns(35)
+
+	if err = db.Ping(); err != nil {
+		log.Fatalln(err) // database is not alive
 	}
 
 	return db
