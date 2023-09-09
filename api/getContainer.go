fb91e85afd3296a2a79bd949d54d2890a9efca4d
362 <643842845@qq.com>
2023-09-09 22:47:51 +0800

    getContainer

--- api/getContainer.go
+++ api/getContainer.go
@@ -1,1 +1,22 @@
 package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"main/database"
+	"main/utils"
+	"net/http"
+	_ "net/http"
+)
+
+func GetContainer(c *gin.Context) {
+	var headerInfo utils.HeaderInfo
+	c.BindHeader(&headerInfo)
+	log.Printf("Get container: %+v\n", headerInfo)
+
+	// Get container list for current user.
+	containerList := database.GetContainersByUser(headerInfo.Username)
+
+	c.JSON(http.StatusOK, containerList)
+
+}
